085eb8188ce85bd464fecfa8c14b6469b8b880c2
Antonia Schmidt-Lademann <totee.schm_lad@yahoo.de>
2018-08-05 21:46:55 +0200

    Parametrise url
    The url that will be scraped has to be given as argument. This allows
    to use the same code to scrape cats and dogs from the spa website and,
    if the use case ever comes up, other animals or even cats and dogs
    at the same time.

--- spa/spa/spiders/dogs.py
+++ spa/spa/spiders/dogs.py
@@ -5,10 +5,13 @@ from scrapy import Request
 from spa.items import SpaItem
 
 
-class DogsSpider(scrapy.Spider):
-    name = 'dogs'
+class SpeciesSpider(scrapy.Spider):
+    name = 'crawlSpecies'
     allowed_domains = ['la-spa.fr']
-    start_urls = ['https://www.la-spa.fr/adopter-animaux?field_esp_ce_value=2&field_race_value=&_field_localisation=refuge&_field_distance=All&field_refuge_animal_target_id=All&_field_adresse=&field_departement_refuge_tid=All&field_sexe_value=All&field_taille_value=All&title_1=&field_sauvetage_value=All&_field_age_value=']
+
+    def __init__(self, url='', **kwargs):
+        self.start_urls = [url]
+        super(SpeciesSpider, self).__init__(**kwargs)
 
     def parse(self, response):
         results = response.xpath('//div[@class="block-result-search "]')
@@ -22,8 +25,8 @@ class DogsSpider(scrapy.Spider):
             img = result.xpath('.//img/@src').extract_first()
             short_name = result.xpath('.//div[@class="field-item even"]/text()').extract_first()
             refuge_name = result.xpath('.//span[@class="refuge-name"]/a/text()').extract_first()
-            doggo = {'URL': url, 'img': img, 'short_name': short_name, 'refuge_name': refuge_name}
-            details = Request(url, callback=self.parse_doggo, meta=doggo)
+            animal = {'URL': url, 'img': img, 'short_name': short_name, 'refuge_name': refuge_name}
+            details = Request(url, callback=self.parse_animal, meta=animal)
 
             yield details
 
@@ -32,7 +35,7 @@ class DogsSpider(scrapy.Spider):
             absolute_next_url = "https://www.la-spa.fr" + relative_next_url
             yield Request(absolute_next_url, callback=self.parse)
 
-    def parse_doggo(self, response):
+    def parse_animal(self, response):
         name = response.xpath('//div[@class="field field-name-field-esp-ce field-type-list-text field-label-inline clearfix"]/div[@class="field-items"]/text()').extract_first()
         breed = response.xpath('//div[@class="field field-name-field-race field-type-text field-label-inline clearfix"]/div[@class="field-items"]/div/text()').extract_first()
         size = response.xpath('//div[@class="field field-name-field-taille field-type-list-text field-label-inline clearfix"]/div[@class="field-items"]/div/text()').extract_first()
@@ -40,7 +43,7 @@ class DogsSpider(scrapy.Spider):
         birthday = response.xpath('//div[@class="field field-name-field-date-naissance field-type-text field-label-inline clearfix"]/div[@class="field-items"]/div/text()').extract_first()
         description = response.xpath('//div[@class="field field-name-body field-type-text-with-summary field-label-hidden"]/div/div/p/text()').extract_first()
 
-        image_array = response.xpath('//div[@class="content col-xs-12 col-sm-8 left-bar dog"]//img[contains(@src, "itok")]/@src').extract()
+        image_array = response.xpath('//div[@class="content col-xs-12 col-sm-8 left-bar cat"]//img[contains(@src, "itok")]/@src').extract()
 
         url = response.meta.get('URL')
         img = response.meta.get('img')
